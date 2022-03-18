d6b879ec7c0314b987c3d64d69242b36ff662452
vaibhav <vaibhavvakil1234@gmail.com>
2022-03-18 16:39:28 +0530

    navigation complete

--- src/component/Homepage.js
+++ src/component/Homepage.js
@@ -1,10 +1,43 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React, { useEffect, useState } from "react";
+import Navbar from "./Navbar";
 
 const Homepage = () => {
-  return (
-    <Navbar/>
-  )
-}
+  const [moviedata, setmoviedata] = useState([]);
+  const [loading, setloading] = useState(true);
+  const [search, setsearch] = useState(false);
+  const [searchdata, setsearchdata] = useState("");
+  useEffect(async () => {
+    try {
+      const response = await fetch(`https://randomuser.me/api/?results=15`);
+      const json = await response.json();
+      setloading(false);
 
-export default Homepage
+      setmoviedata(json.results);
+    } catch (e) {
+      alert(`Pls check Your Internet Connection or Refresh Page ${e}`);
+    }
+  }, [setsearchdata]);
+
+  return (
+    <>
+      <Navbar setsearchdata={setsearchdata} setsearch={setsearch} />
+      <div className="lg:grid-col-4 md:grid-col-3 sm:grid-col-2 sm:grid-cols-1">
+        {moviedata
+          ?.filter((movie) => {
+            if (searchdata === "") {
+              return movie;
+            } else if (
+              movie.name.first.toLowerCase().includes(searchdata.toLowerCase())
+            ) {
+              return movie;
+            }
+          })
+          .map((movie) => {
+            return <h1>{movie.name.first}</h1>;
+          })}
+      </div>
+    </>
+  );
+};
+
+export default Homepage;
--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import logo from "./logo.png";
 
-const Navbar = () => {
+const Navbar = ({setsearchdata,setsearch}) => {
   return (
-    <div>Navbara</div>
-  )
-}
+    <div className="flex h-20 border-2 items-center shadow justify-between">
+      <div className="Item1 ml-28">
+        <img src={logo} className="h12 w-12" />
+      </div>
+      <div className="itme2  border shadow-md h-10 rounded-md justify-center items-center flex w-56 mr-24">
+        <input
+          type="text"
+          className="border-none "
+          placeholder="Search user...."
+          onChange={event => { setsearchdata(event.target.value); setsearch(true) }}
+        ></input>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
